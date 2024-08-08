064b5e597757a1002c161f2b1e90e36420923fc6
Katy Kahla <katykahla@gmail.com>
2024-08-08 13:37:59 -0600

    cleaned up some padding

--- marketplace-ui/src/components/JobLayout.jsx
+++ marketplace-ui/src/components/JobLayout.jsx
@@ -37,7 +37,7 @@ const LoadingView = () => (
 );
 
 const JobView = ( {jobs} ) => (
-<Grid container spacing={2} justifyContent="center">
+<Grid container spacing={2} justifyContent="center" style={{paddingLeft: 16, paddingRight: 16}}>
   {jobs.map((job) => (
     <Grid item xs={12} sm={6} md={4} lg={3} key={job.id}>
       <JobCard job={job} />
@@ -97,7 +97,7 @@ export default function JobLayout() {
 
   return (
     <>
-    <Grid container spacing={2} alignItems="center" style={{ marginBottom: 20 }}>
+    <Grid container spacing={2} alignItems="center" style={{ marginBottom: 20, padding: 16 }}>
       <Grid item xs={12} sm={3}></Grid>
       <Grid item xs={12} sm={6} container justifyContent="center">
         <ToggleButtonGroup
